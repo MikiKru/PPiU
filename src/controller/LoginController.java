297d9dc871fd766259e49126956ffbe97bfb2e4e
MikiKru <michal_kruczkowski@o2.pl>
2019-03-19 09:51:36 +0100

    LECT 2

---
+++ src/controller/LoginController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+public class LoginController {
+
+    @FXML
+    private TextField tf_login;
+
+    @FXML
+    private HBox pf_password;
+
+    @FXML
+    void loginAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerAction(ActionEvent event) {
+
+    }
+
+}
